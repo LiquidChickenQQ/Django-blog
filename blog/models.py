b637b31c11b200a0d93199667d176a3fd6eedfd2
Kerry Bodle <kerrybodle@yahoo.com>
2019-03-11 15:17:52 -0400

    added styling

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from embed_video.fields import EmbedVideoField
@@ -8,7 +7,7 @@ from embed_video.fields import EmbedVideoField
 class Post(models.Model):
     title = models.CharField(max_length=100)
     video = EmbedVideoField(default='Enter a URL')
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
